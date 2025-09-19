965aa0e69a25f861512cfdf84cd442887d1711e2
Priyanka Manjare <priyankamanjare05@gmail.com>
2025-09-19 19:59:04 +0530

    Changed assests

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import sunnyBg from "@/assets/sunny.jpeg"
+import defaultBg from "@/assets/default.jpg"
 import nightBg from "@/assets/night-bg.jpg"
 import cloudBg from "@/assets/cloud-bg.jpeg"
 import rainBg from "@/assets/rain-bg.jpeg"
@@ -38,9 +39,9 @@ const Weather = () => {
     const getWeatherStyle = (condition, isDay) => {
         if (condition.includes("sunny") && isDay) return sunnyBg;
         if (condition.includes("clear") && !isDay) return nightBg;
-        if (condition.includes("mist") || condition.includes("fog") || condition.includes("haze")) return mistBg;        if (condition.includes("cloud")) return cloudBg;
+        if (condition.includes("mist") || condition.includes("fog") || condition.includes("haze")) return mistBg; if (condition.includes("cloud")) return cloudBg;
         if (condition.includes("rain")) return rainBg;
-        
+
         return sunnyBg;
     }
 
@@ -52,38 +53,42 @@ const Weather = () => {
     //      })
     //   },[city])
 
+
+    // Determine background image
+    const backgroundImage = weather
+        ? `url(${getWeatherStyle(
+            weather.current.condition.text.toLowerCase(),
+            weather.current.is_day
+        )})`
+        : `url(${defaultBg})`;
+
     return (
         <div className='p-0'>
             <div
-                className="w-full h-screen  bg-cover bg-center p-10"
+                className="w-full h-screen bg-cover bg-center p-10 flex items-center justify-center"
                 style={{
-                    backgroundImage: weather
-                        ? `url(${getWeatherStyle(
-                            weather.current.condition.text.toLowerCase(),
-                            weather.current.is_day
-                        )})`
-                        : "none"
+                    backgroundImage: backgroundImage
                 }}
             >
-                <div className='flex flex-col items-center justify-center p-4 text-slate-50
-                h-full w-full bg-white-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-70 border border-gray-100'>
+                <div className='flex flex-col items-center justify-center p-4 text-slate-50 lg:w-1/2
+                lg:h-full bg-white-700 rounded-md bg-clip-padding backdrop-opacity-100 backdrop-blur-sm bg-opacity-70 border border-gray-100'>
                     <input type='text'
                         placeholder='Enter a city'
                         value={city}
                         onChange={e => setCity(e.target.value)}
-                        className='bg-slate-50 rounded-4xl text-slate-800 px-2 border-2 border-amber-200' />
+                        className='bg-slate-50 rounded-4xl text-slate-500 px-2 border-2 border-amber-200' />
                     <button
                         onClick={fetchWeather}
-                        className=' text-slate-50 bg-amber-200 m-4 rounded-2xl px-3 py-1 transition-transform duration-300 ease-out hover:scale-110 border-4 border-slate-300'>Search</button>
+                        className=' text-slate-50 bg-amber-400 m-4 rounded-2xl px-3 py-1 transition-transform duration-300 ease-out hover:scale-110 border-4 border-slate-300'>Search</button>
 
                     {loading && <p>Loading...</p>}
                     {error && <p className='text-red-600'>{error}</p>}
                     {weather && !loading && !error && (
                         <div>
-                            <h2>{weather.location.name} : {weather.location.country} : {weather.location.region}</h2>
-                            <div>
-                                <div className='flex justify-center items-center'>
-                                    <h5>{weather.current.condition.text}</h5>
+                            <h2 className='font-bold'>{weather.location.name} : {weather.location.country} : {weather.location.region}</h2>
+                            <div className='text-xs md:text-lg lg:font-extrabold'>
+                                <div className='flex justify-start items-center'>
+                                    <h2 className='lg:text-xl'>{weather.current.condition.text}</h2>
                                     <img src={weather.current.condition.icon} alt='weather icon' />
                                 </div>
                                 <h4>Temperature(in celcius) : {weather.current.temp_c}</h4>
